03bc364e5069871cd79b9b2c5fff6f16fb6b24ef
엄태권 <akfls221@gmail.com>
2022-10-24 03:22:16 +0900

    fix : BlackShip and factory 생성

---
+++ src/main/java/com/example/designpatterns/factorymethod/BlackShip.java
@@ -0,0 +1,10 @@
+package com.example.designpatterns.factorymethod;
+
+public class BlackShip extends Ship{
+
+    public BlackShip() {
+        setColor("black");
+        setLogo("닻");
+        setName("blackShip");
+    }
+}
