a39c577d3dfa85bfe0b224dc164cd76a8a0ec293
엄태권 <akfls221@gmail.com>
2022-10-24 03:21:46 +0900

    fix : white ship and factory 생성

---
+++ src/main/java/com/example/designpatterns/factorymethod/WhiteShip.java
@@ -0,0 +1,10 @@
+package com.example.designpatterns.factorymethod;
+
+public class WhiteShip extends Ship{
+
+    public WhiteShip() {
+        setName("whiteShip");
+        setColor("white");
+        setLogo("uD83D\uDEE5");
+    }
+}
