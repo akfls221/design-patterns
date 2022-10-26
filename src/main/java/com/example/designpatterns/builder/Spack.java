664021d3a3f8624233f0fcdfb8f7f7f3d0e3883a
엄태권 <akfls221@gmail.com>
2022-10-27 02:52:23 +0900

    feat : spack 도메인 추가

---
+++ src/main/java/com/example/designpatterns/builder/Spack.java
@@ -0,0 +1,25 @@
+package com.example.designpatterns.builder;
+
+public class Spack {
+
+    private int power;
+
+    private int brain;
+
+    private int speed;
+
+    public Spack(int power, int brain, int speed) {
+        this.power = power;
+        this.brain = brain;
+        this.speed = speed;
+    }
+
+    @Override
+    public String toString() {
+        return "Spack{" +
+                "power=" + power +
+                ", brain=" + brain +
+                ", speed=" + speed +
+                '}';
+    }
+}
