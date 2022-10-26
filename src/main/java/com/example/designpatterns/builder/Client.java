07c76096b8c7bb373c0e1c32b5b272448614c213
엄태권 <akfls221@gmail.com>
2022-10-27 02:53:48 +0900

    feat : Client 추가

---
+++ src/main/java/com/example/designpatterns/builder/Client.java
@@ -0,0 +1,33 @@
+package com.example.designpatterns.builder;
+
+import java.time.LocalDateTime;
+
+public class Client {
+
+    public static void main(String[] args) {
+        CharacterBuilder builder = new DefaultCharacterBuilder();
+
+        Spack spack = new Spack(100, 100, 100);
+        Spack spack2 = new Spack(50, 50, 50);
+
+
+        Character character = builder
+                .name("엄태권")
+                .age(29)
+                .birthDay("2021-10-27")
+                .createDate(LocalDateTime.now())
+                .spackList(spack)
+                .spackList(spack2)
+                .getCharacter();
+
+        System.out.println("character = " + character);
+
+        /**
+         * 빌더를 만들어둔 Director 사용방법
+         */
+        CharacterDirector characterDirector = new CharacterDirector(new DefaultCharacterBuilder());
+        Character taeKwanCharacter = characterDirector.createTaeKwanCharacter();
+
+        System.out.println("taeKwanCharacter = " + taeKwanCharacter);
+    }
+}
