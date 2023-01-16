4be5d46b56cd2483dd565b65cd7144c1eb827b3d
엄태권 <akfls221@gmail.com>
2023-01-17 01:24:10 +0900

    feat : Champion 구현

--- src/main/java/com/example/designpatterns/bridge/Champion.java
+++ src/main/java/com/example/designpatterns/bridge/Champion.java
@@ -1,4 +1,16 @@
 package com.example.designpatterns.bridge;
 
 public interface Champion {
+
+    void skillQ();
+
+    void skillW();
+
+    void skillE();
+
+    void skillR();
+
+    default void moveRight() {
+
+    }
 }
