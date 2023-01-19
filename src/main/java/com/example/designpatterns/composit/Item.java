ff9d9d942801699cd88cf42cace0d7cc49d326b0
엄태권 <akfls221@gmail.com>
2023-01-20 02:06:59 +0900

    feat : Component interface 를 상속받는 컴포짓 객체 또는 leaf(item) 객체 생성

---
+++ src/main/java/com/example/designpatterns/composit/Item.java
@@ -0,0 +1,22 @@
+package com.example.designpatterns.composit;
+
+public class Item implements Component{
+
+    private String name;
+
+    private int price;
+
+    public Item(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public int getPrice() {
+        return price;
+    }
+}
