a07e5442c105af10d6c2ce66da5fb52b95bb625f
엄태권 <akfls221@gmail.com>
2023-01-23 02:19:56 +0900

    feat : client 및 app 코드 생성

---
+++ src/main/java/com/example/designpatterns/decorator/Client.java
@@ -0,0 +1,14 @@
+package com.example.designpatterns.decorator;
+
+public class Client {
+
+    private CommentService commentService;
+
+    public Client(CommentService commentService) {
+        this.commentService = commentService;
+    }
+
+    public void writeComment(String comment) {
+        commentService.addComment(comment);
+    }
+}
