abdf00470555a38ed66adb138d425abe09c5b4e2
엄태권 <akfls221@gmail.com>
2023-01-24 03:26:55 +0900

    feat : EmailMessage Class 생성

---
+++ src/main/java/com/example/designpatterns/facacde/EmailMessage.java
@@ -0,0 +1,44 @@
+package com.example.designpatterns.facacde;
+
+public class EmailMessage {
+
+    private String from;
+
+    private String to;
+
+    private String title;
+
+    private String text;
+
+    public String getFrom() {
+        return from;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public void setTo(String to) {
+        this.to = to;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
