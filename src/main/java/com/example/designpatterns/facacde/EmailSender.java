4fa11054a42409a3b6e8be49fb7b2532203bed4f
엄태권 <akfls221@gmail.com>
2023-01-24 16:43:05 +0900

    feat : code 정리

--- src/main/java/com/example/designpatterns/facacde/EmailSender.java
+++ src/main/java/com/example/designpatterns/facacde/EmailSender.java
@@ -1,6 +1,10 @@
 package com.example.designpatterns.facacde;
 
+import javax.mail.Message;
+import javax.mail.MessagingException;
 import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 import java.util.Properties;
 
@@ -17,6 +21,18 @@ public class EmailSender {
         properties.setProperty("mail.smtp.host", emailSettings.getHost());
 
         Session session = Session.getDefaultInstance(properties);
+
+        try {
+            MimeMessage message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(emailMessage.getFrom()));
+            message.addRecipients(Message.RecipientType.TO, String.valueOf(new InternetAddress(emailMessage.getTo())));
+            message.setSubject(emailMessage.getTitle());
+            message.setText(emailMessage.getText());
+
+            Transport.send(message);
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
 
     }
 }
